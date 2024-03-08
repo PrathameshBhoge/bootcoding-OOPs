56bc419004e83b42ee1cd39f70b6485a673edf29
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-03-09 00:28:54 +0530

    OOP's

---
+++ src/main/java/com/bootcoding/inheritance/meter/meter.java
@@ -0,0 +1,15 @@
+package com.bootcoding.inheritance.meter;
+
+public class meter {
+    String material;
+    String Volts;
+    double price;
+    double reading;
+    String type;
+    public void buildMeter(){
+        System.out.println("Build a meter of following specification. ");
+    }
+    public void Display(){
+        System.out.println("This are the meter details. ");
+    }
+}
