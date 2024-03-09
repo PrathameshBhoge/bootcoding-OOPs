cbe9debb441c9b24655dfc85608d9afd02b880c1
PrathameshBhoge <prathameshbhoge230@gmail.com>
2024-03-09 23:24:21 +0530

    OOP's

---
+++ src/main/java/com/bootcoding/inheritance/banner/FlexBanner.java
@@ -0,0 +1,10 @@
+package com.bootcoding.inheritance.banner;
+
+ class FlexBanner extends Banner{
+
+   FlexBanner(){
+    System.out.println("Create A flex banner.");
+   }
+   // contructor can be
+
+}
